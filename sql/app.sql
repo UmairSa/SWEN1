52701628ded0fbdb07980bd03e66bb184f02047a
Umair Saqub <if22b029@technikum-wien.at>
2024-01-15 18:48:00 +0100

    DB? idk

---
+++ sql/app.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE mtcgdb;
+
+DROP TABLE users CASCADE;
+CREATE TABLE IF NOT EXISTS users
+(
+    userid   SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)        NOT NULL,
+    coins    INT DEFAULT 20,
+    elo      INT DEFAULT 100,
+    name     varchar(255),
+    bio      varchar(255),
+    image    varchar(255),
+    wins     INT DEFAULT 0,
+    losses   INT DEFAULT 0
+);
+
+DROP TABLE packages cascade;
+CREATE TABLE IF NOT EXISTS packages
+(
+    packageid INT PRIMARY KEY,
+    price     INT DEFAULT 5 NOT NULL
+);
+
+drop table cards cascade;
+CREATE TABLE IF NOT EXISTS cards
+(
+    cardid      UUID PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    damage      DECIMAL NOT NULL,
+    elementtype VARCHAR(255) CHECK (elementtype IN ('Fire', 'Water', 'Normal')),
+    cardtype    VARCHAR(255) CHECK (cardtype IN ('Monster', 'Spell')),
+    indeck      BOOLEAN DEFAULT FALSE,
+    ownerid     INT REFERENCES users (userid) ON DELETE SET NULL,
+    packageid   INT REFERENCES packages (packageid) ON DELETE SET NULL
+);
+
+drop table trade cascade;
+CREATE TABLE IF NOT EXISTS trade
+(
+    tradeid          UUID PRIMARY KEY,
+    cardtotradeid    UUID NOT NULL REFERENCES cards (cardid),
+    cardtype         VARCHAR(255),
+    minimumdamage    DECIMAL,
+    user1id          INT NOT NULL REFERENCES users (userid),
+    user2id          INT REFERENCES users (userid)
+);
+
+drop table battle cascade;
+CREATE TABLE IF NOT EXISTS battle
+(
+    battleid     SERIAL PRIMARY KEY,
+    player1id    INT NOT NULL REFERENCES users (userid),
+    player2id    INT NOT NULL REFERENCES users (userid),
+    battlelog    TEXT,
+    battleoutcome VARCHAR(255),
+    createdat    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
